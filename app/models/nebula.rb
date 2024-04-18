a94d09aabfc38294254c6e527de90a4c63d8be54
Conan Duval-Manahan <conanduval-manahan@outlook.com>
2024-04-18 21:56:16 +0100

    Nebula Model Expanded Including New Method, Galaxy Model Created
    Intention is to gradually expand the scope of what Nebula means to the
    application, initially in this commit it is to start the growth of Nebula
    with attributes and associations, with a distance method also included
    and comments to clarify its existence.
    
    Galaxy model also created for an initial belongs_to nebula association from
    nebula to galaxy in the nebula model.

--- app/models/nebula.rb
+++ app/models/nebula.rb
@@ -1,3 +1,12 @@
 class Nebula < ApplicationRecord
+  belongs_to :galaxy
   has_one_attached :photo
+  attribute :name, :string
+  attribute :distance, :float
+  attribute :brightness, :integer
+
+  # Future method to implement
+  def visible_from_earth?
+    distance < 1000 # Determined threshold for visibility in km, for example Orion
+  end
 end
