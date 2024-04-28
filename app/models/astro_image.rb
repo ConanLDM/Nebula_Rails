52604b718e59aa8d6803833a0ab9fe7d4ea5b225
Conan Duval-Manahan <conanduval-manahan@outlook.com>
2024-04-28 13:30:12 +0100

    Added a Description Character Limit, Message in the View Also
    For the astro_image model and the astro_image form:
    
    Added a description character limit of 200 characters, if over 200 then
    the user will get a message thatr "200 is the maxmimum allowed".
    
    Also added a title validation rule in the astro_image model

--- app/models/astro_image.rb
+++ app/models/astro_image.rb
@@ -1,3 +1,6 @@
 class AstroImage < ApplicationRecord
   has_one_attached :image
+  validates :title, presence: true
+  validates :description, length: { maximum: 200,
+    too_long: "%{count} characters is the maximum allowed" }
 end
